2b65ed3d72a514735be1e9a526f42310a5181cc8
Omjavir <javirom07042003@gmail.com>
2025-02-23 11:42:34 +0530

    Add: added Lombok dependency which helps reduce the getter setter code

--- src/main/java/com/example/Todo/entity/TodoEntry.java
+++ src/main/java/com/example/Todo/entity/TodoEntry.java
@@ -1,42 +1,19 @@
 package com.example.Todo.entity;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
+@Getter
+@Setter
 public class TodoEntry {
+
 
     @Id
     private ObjectId id;
     private String title;
     private String description;
-
-    // Default constructor (optional, but good to have)
-    public TodoEntry() {}
-
-    // Getters and setters
-    public ObjectId getId() {
-        return id;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
